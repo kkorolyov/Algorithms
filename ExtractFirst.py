e313a7bd6a9b55b7abf4c1c67b517560b3392d00
kkorolyov <kirillk753@gmail.com>
2017-03-12 03:59:53 -0700

    Add ExtractFirst

---
+++ ExtractFirst.py
@@ -0,0 +1,21 @@
+# Extracts the first occurrence of each distinct character in a string
+
+def main():
+	print("Input: ", end='')
+	string = input()
+
+	print("First distinct characters: {0}".format(extractFirsts(string)))
+
+def extractFirsts(string):
+	firsts = ""
+	found = set()
+
+	for c in string:
+		if c not in found:
+			found.add(c)
+			firsts += c
+	
+	return firsts
+
+if __name__ == "__main__":
+	main()
