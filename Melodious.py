00b2eed3ee4ddca761614a10700e5df1ad1f3ecc
kkorolyov <kirillk753@gmail.com>
2017-03-15 14:51:45 -0700

    Rename

--- Melodious.py
+++ Melodious.py
@@ -1,31 +1,31 @@
-# Displays all melodious passwords of length n
+# Displays all melodious (alternating consonants and vowels) words of length n
 
 def main():
 	print("Password length: ", end='')
 	n = int(input().strip())
 
-	passwords = melodious(n)
+	words = melodious(n)
 
-	for password in passwords:
-		print(password)
+	for word in words:
+		print(word)
 
 def melodious(n):
 	vowels = {'a', 'e', 'i', 'o', 'u'}
 	consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'}
 
-	def makePass(n, lastVowel):
+	def makeWord(n, lastVowel):
 		results = list(consonants if lastVowel else vowels)
 
 		if n > 1:
 			oldResults = results
 			results = []
 			for result in oldResults:
-				for newResult in makePass(n - 1, not lastVowel):
+				for newResult in makeWord(n - 1, not lastVowel):
 					results.append(result + newResult)
 
 		return results
 
-	return makePass(n, True) + makePass(n, False)
+	return makeWord(n, True) + makeWord(n, False)
 
 if __name__ == '__main__':
 	main()
