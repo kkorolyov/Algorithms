7c32949d44fff9d258671691a51d83d27d734e6d
kkorolyov <kirillk753@gmail.com>
2017-04-10 09:16:37 -0700

    Add ValidWord

---
+++ ValidWord.py
@@ -0,0 +1,17 @@
+def main():
+	word = input().strip()
+
+	print("Yes" if valid(word) else "No")
+
+def valid(word):
+	word = word.upper()
+	vowels = ("A", "E", "I", "O", "U", "Y")
+
+	for i in range(1, len(word)):
+		if (word[i] == word[i - 1]) or (word[i] in vowels and word[i - 1] in vowels):
+			return False
+
+	return True
+
+if __name__ == "__main__":
+	main()
